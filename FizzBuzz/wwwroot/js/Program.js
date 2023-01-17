0f971f2089a0b3d4bd9fe4f325fea561fcabbf37
Rida-Abid <107047216+Rida-Abid@users.noreply.github.com>
2023-01-17 20:50:07 +0500

    Added code for user input , for values fizzId, buzzId, maxCount. Addeded Logic module

---
+++ FizzBuzz/wwwroot/js/Program.js
@@ -0,0 +1,17 @@
+﻿
+class Program {
+    MainMethod() {
+        var args = arguments;
+        if (arguments.length != 3) {
+            console.error("Please enter 3 values");
+        }
+        else {
+            var fizzId, buzzId, maxCount = arguments;
+
+        }
+
+
+    }
+     
+
+}

fff04c73a8b3cecf93d24090695c8bfa01fd5fb9
Rida-Abid <107047216+Rida-Abid@users.noreply.github.com>
2023-01-18 21:10:33 +0500

    Added code to Logic module for main functioning of the program.

--- FizzBuzz/wwwroot/js/Logic.js
+++ FizzBuzz/wwwroot/js/Logic.js
@@ -1,6 +1,35 @@
-﻿namespace FizzBuzz.wwwroot.js
-{
-    public class Logic
-    {
+﻿
+class Logic {
+     ProcessMethod(fizzId, buzzId, maxCount) {
+         var result = [];
+         if ((fizzId < 1) || (buzzId < 1) || (maxCount < 1));
+         result.append("Enter all numbers greater than 0");
+         return result;
+
+         for (let i = 0; i < maxCount; i++) {
+             result.append(this.CalcMethod(i, fizzId, buzzId));
+             return result;
+         }
+     }
+
+    CalcMethod(i, fizzId, buzzId) {
+
+        if ((i % fizzId == 0) && (i % buzzId)) {
+            return "fizzbuzz";
+        }
+        else if (i % fizzId == 0) {
+            return "fizz";
+        }
+        else if (i % buzzId) {
+            return "buzz";
+        }
+        else{
+            return i;
+        }
+
+        
     }
+
 }
+
+
--- FizzBuzz/wwwroot/js/Program.js
+++ FizzBuzz/wwwroot/js/Program.js
@@ -1,17 +1,27 @@
-﻿
-class Program {
-    MainMethod() {
-        var args = arguments;
+﻿var arr = [];
+arr = prompt("Enter your numbers").split("");
+function Main(arr)
+{
+  
+
         if (arguments.length != 3) {
             console.error("Please enter 3 values");
+            console.log("Usage: fizzId, buzzId, maxCount");
         }
         else {
-            var fizzId, buzzId, maxCount = arguments;
+            var fizzId, buzzId, maxCount = arr[0]; [1], [2];
+            fizzId = parseInt(fizzId);
+            buzzId = parseInt(buzzId);
+            maxCount = parseInt(maxCount);
+            const logic = new Logic()
+            var results = logic.Process(fizzId, buzzId, maxCount);
+            result.foreach(Process)
+            console.log(result);
+            
 
+
+                
         }
-
-
-    }
      
 
 }

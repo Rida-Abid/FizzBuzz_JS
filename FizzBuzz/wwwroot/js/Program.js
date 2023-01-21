b23c07f68caf5d91b6843da39b8d271e06c93c05
Rida-Abid <107047216+Rida-Abid@users.noreply.github.com>
2023-01-22 00:43:12 +0500

    Removed the code from Logic module and added the whole code to Program module  Redesigned FizzBuzz by removing all errors.

--- FizzBuzz/wwwroot/js/Program.js
+++ FizzBuzz/wwwroot/js/Program.js
@@ -1,27 +1,49 @@
-﻿var arr = [];
-arr = prompt("Enter your numbers").split("");
+﻿
+
+var arr = [];
+arr = prompt("Enter your numbers").split(" ");
+Main(arr);
+
 function Main(arr)
 {
-  
+    if (arr.length != 3) {
+        console.error("Please enter 3 values");
+        console.log("Usage: fizzId, buzzId, maxCount");
+    }
+    else {
+        var fizzId = parseInt(arr[0]);
+        var buzzId = parseInt(arr[1]);
+        var maxCount = parseInt(arr[2]);
+        var result = [];
 
-        if (arguments.length != 3) {
-            console.error("Please enter 3 values");
-            console.log("Usage: fizzId, buzzId, maxCount");
+        function Process(fizzId, buzzId, maxCount) {
+
+            for (var i = 1; i <= maxCount; i++) {
+                result.push(Calc(i, fizzId, buzzId));
+
+            }
+            return result;
         }
-        else {
-            var fizzId, buzzId, maxCount = arr[0]; [1], [2];
-            fizzId = parseInt(fizzId);
-            buzzId = parseInt(buzzId);
-            maxCount = parseInt(maxCount);
-            const logic = new Logic()
-            var results = logic.Process(fizzId, buzzId, maxCount);
-            result.foreach(Process)
-            console.log(result);
-            
 
+        function Calc(i, fizzId, buzzId) {
 
-                
+            if ((i % fizzId == 0) && (i % buzzId == 0)) {
+                return "fizzbuzz";
+            }
+            else if (i % fizzId == 0) {
+                return "fizz";
+            }
+            else if (i % buzzId == 0) {
+                return "buzz";
+            }
+            else {
+                return (i);
+            }
         }
+
+        
+        console.log(Process(fizzId, buzzId, maxCount));
+    }
      
 
 }

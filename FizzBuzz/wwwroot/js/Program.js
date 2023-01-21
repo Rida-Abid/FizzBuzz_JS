96e884c37e2e90e38bf69513fc9cba50f5f243cd
Rida-Abid <107047216+Rida-Abid@users.noreply.github.com>
2023-01-22 00:50:41 +0500

    Added code for If user inputs less than 3 values. If user enters values less than 1.

--- FizzBuzz/wwwroot/js/Program.js
+++ FizzBuzz/wwwroot/js/Program.js
@@ -10,40 +10,47 @@ function Main(arr)
         console.error("Please enter 3 values");
         console.log("Usage: fizzId, buzzId, maxCount");
     }
+
     else {
         var fizzId = parseInt(arr[0]);
         var buzzId = parseInt(arr[1]);
         var maxCount = parseInt(arr[2]);
         var result = [];
 
-        function Process(fizzId, buzzId, maxCount) {
-
-            for (var i = 1; i <= maxCount; i++) {
-                result.push(Calc(i, fizzId, buzzId));
-
-            }
-            return result;
+        if ((fizzId < 1) || (buzzId < 1) || (maxCount < 1)) {
+            console.log("Enter all numbers greater than 0");
+            return;
         }
 
-        function Calc(i, fizzId, buzzId) {
+        else {
+            function Process(fizzId, buzzId, maxCount) {
 
-            if ((i % fizzId == 0) && (i % buzzId == 0)) {
-                return "fizzbuzz";
+                for (var i = 1; i <= maxCount; i++) {
+                    result.push(Calc(i, fizzId, buzzId));
+
+                }
+                return result;
             }
-            else if (i % fizzId == 0) {
-                return "fizz";
+
+            function Calc(i, fizzId, buzzId) {
+
+                if ((i % fizzId == 0) && (i % buzzId == 0)) {
+                    return "fizzbuzz";
+                }
+                else if (i % fizzId == 0) {
+                    return "fizz";
+                }
+                else if (i % buzzId == 0) {
+                    return "buzz";
+                }
+                else {
+                    return (i);
+                }
             }
-            else if (i % buzzId == 0) {
-                return "buzz";
-            }
-            else {
-                return (i);
-            }
+            console.log(Process(fizzId, buzzId, maxCount));
         }
-
-
         
-        console.log(Process(fizzId, buzzId, maxCount));
+        
     }
      
 
